fe1723f055d1d497c91dc54fb229e9e4cd807274
mateusgpontes <mateus.gpontes@hotmail.com>
2020-08-12 16:03:44 -0300

    creating logic for the game answers me

--- app/src/main/java/com/example/projeto/MainActivity.kt
+++ app/src/main/java/com/example/projeto/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.projeto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+fun resMenssage(): String {
+
+    val res = arrayOf("Sim", "Não", "Talves")
+    val randow = Random.Default
+    val index: Int = randow.nextInt(3)
+
+    return res[index]
+}
+
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textDefault = "Faça sua pergunta"
+
+        editText.setOnFocusChangeListener { v, hasFocus ->
+            textView.text = textDefault
+        }
+
+        button.setOnClickListener {
+            if(editText.text.isNotEmpty()){
+                textView.text = resMenssage()
+            }else{
+                textView.text = textDefault
+            }
+            editText.text = null
+        }
     }
 }
